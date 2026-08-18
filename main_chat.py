e6291a2050a81683433ebc6310e843b56f43f49b
caiobfreitasdev <caiobrenod2@hotmail.com>
2026-08-18 17:49:08 -0300

    Alterações no chat, criando template prompt

--- main_chat.py
+++ main_chat.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 load_dotenv()
 api_key = os.getenv("api_key_open_ai")
 
@@ -11,14 +12,23 @@ modelo = ChatOpenAI(
     api_key=api_key
 )
 
+prompt_sugestao = ChatPromptTemplate.from_messages(
+    [
+        ("system", "Você é um guia de viagem especializado em destinos brasileiros. Apresente-se como Sr. Passeios"),
+        ("placeholder", "{historico}"),
+        ("human", "{query}")   
+    ]
+)
+
+cadeia = prompt_sugestao | modelo | StrOutputParser()
+
 lista_de_perguntas = [
     "Sugira uma cidade para visitar dado o meu interesse por praias e cultura.",
-    "Sugira restaurantes populares",
-    "Sugira a melhor época do ano para visitar a cidade sugerida"
+    "Qual melhor época do ano para ir?"
     ]
 
 for pergunta in lista_de_perguntas:
     resposta = modelo.invoke(pergunta)
-    print("User: ", pergunta)
+    print("User: ", pergunta, "\n")
     print("IA: ", resposta.content, "\n")
     
