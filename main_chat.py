f5aba5b0e01e22d740e9caec480556b9b5a1655b
caiobfreitasdev <caiobrenod2@hotmail.com>
2026-08-18 17:55:01 -0300

    Ajustes na memoria do chat

--- main_chat.py
+++ main_chat.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
 load_dotenv()
 api_key = os.getenv("api_key_open_ai")
 
@@ -22,13 +25,35 @@ prompt_sugestao = ChatPromptTemplate.from_messages(
 
 cadeia = prompt_sugestao | modelo | StrOutputParser()
 
+
+memoria = {}
+sessao = "aula_langchain_alura"
+
+def historico_por_sessao(sessao : str):
+    if sessao not in memoria:
+        memoria[sessao] = InMemoryChatMessageHistory()
+    return memoria[sessao]
+
 lista_de_perguntas = [
     "Sugira uma cidade para visitar dado o meu interesse por praias e cultura.",
     "Qual melhor época do ano para ir?"
     ]
 
+cadeia_com_memoria = RunnableWithMessageHistory(
+    runnable=cadeia,
+    get_session_history=historico_por_sessao,
+    input_messages_key="query",
+    history_messages_key="historico"
+)
+
+
 for pergunta in lista_de_perguntas:
-    resposta = modelo.invoke(pergunta)
+    resposta = cadeia_com_memoria.invoke(
+        {
+            "query": pergunta
+        },
+        config={"session_id":sessao}
+    )
     print("User: ", pergunta, "\n")
-    print("IA: ", resposta.content, "\n")
+    print("IA: ", resposta, "\n")
     
